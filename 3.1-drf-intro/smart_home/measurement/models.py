3a2efd5148bca2b82c51ea47afb8bd604502a490
VladIakimenko <v_yakimenko@inbox.ru>
2023-05-20 12:42:56 +0400

    drf-intro

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Reading(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.PROTECT, related_name='measurements')
+    temperature = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(null=True)
+
+    def __str__(self):
+        return f'{self.sensor.name}:{self.created_at}'
